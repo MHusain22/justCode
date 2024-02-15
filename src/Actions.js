6acd7036874dee2b1215997b900acff3bb25faf0
MHusain22 <husain.vijapura@gmail.com>
2024-02-15 11:35:19 +0530

    first commit

---
+++ src/Actions.js
@@ -0,0 +1,12 @@
+const ACTIONS = {
+    JOIN: 'join',
+    JOINED: 'joined',
+    DISCONNECTED: 'disconnected',
+    CODE_CHANGE: 'code-change',
+    SYNC_CODE: 'sync-code',
+    LEAVE: 'leave',
+};
+
+export default ACTIONS;
+//we can write it directly 
+//we created this because to prevent typo mistakes
